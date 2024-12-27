de172c1bec4c319b514b55ef5a7271061ebe1318
Xclusivkhodes <xclusivkhoder@gmail.com>
2024-12-27 13:50:21 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const App = () => (
+  <main className='relative'>
+    <Nav>nav</Nav>
+    <Section className = 'xl:padding-1 wide: padding-r padding-b'>
+      <Hero>hero</Hero>
+    </Section>
+    <Section className = 'padding'>
+      Popular Products
+    </Section>
+    <Section className = 'padding'>
+      SuperQuality
+    </Section>
+    <Section className = 'padding'>
+      Services
+    </Section>
+    <Section className = 'padding'>
+      SpecialOffers
+    </Section>
+  </main>
+)
+
+export default App
